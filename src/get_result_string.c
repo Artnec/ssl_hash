3d5683e015c1eeca29c63ac152c5fa65ad62f600
Artem NESTERUK <anesteru@e2r5p3.unit.ua>
2018-12-14 21:35:09 +0200

    replaced magic numbers with character literals

--- src/get_result_string.c
+++ src/get_result_string.c
@@ -6,9 +6,9 @@ char		*get_result_string(unsigned char *result, int size)
 	for (int i = 0; i < size; i++)
 	{
 		char c = result[i] >> 4;
-		result_str[i * 2] = (c < 0xa ? c + 48 : c + 87);
+		result_str[i * 2] = (c < 0xa ? c + '0' : c + 'a' - 10);
 		c = result[i] & 0xf;
-		result_str[i * 2 + 1] = (c < 0xa ? c + 48 : c + 87);
+		result_str[i * 2 + 1] = (c < 0xa ? c + '0' : c + 'a' - 10);
 	}
 	result_str[size * 2] = '\0';
 	return (result_str);
